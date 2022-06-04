1df5958971fc4d99a5d719a4ac17e4007985c809
heutlett <44324449+Heutlett@users.noreply.github.com>
2022-06-04 01:49:01 -0600

    se agrega un resumen de los modulos del proyecto

--- LIBRO/decoder.sv
+++ LIBRO/decoder.sv
@@ -18,11 +18,11 @@ module decoder
 										// Data-processing immediate
 			2'b00: if (Funct[5]) controls = 10'b0000101001;
 										// Data-processing register
-			else 						controls = 10'b0000001001;
+			else 						controls = 10'b0000001001;   
 										// LDR
-			2'b01: if (Funct[0]) controls = 10'b0001111000;
-										// STR
-			else 						controls = 10'b1001110100;
+			2'b01: if (Funct[0]) controls = 10'b0001111000;  //op = 01, L=1    // FALTA IMPLEMENTAR "B" FUNCT[2]
+										// STR												    // PARA AGREGAR EL STORE BYTE
+			else 						controls = 10'b1001110100;  //op = 01, L=0
 										// B
 			2'b10: 					controls = 10'b0110100010;
 										// Unimplemented
