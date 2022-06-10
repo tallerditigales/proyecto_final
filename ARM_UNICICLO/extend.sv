01162ab7c27c7a16b25b7ba14d376b24cccac9cf
alejandrodp <diaza913@gmail.com>
2022-06-10 17:28:48 -0600

    Completa rotación de inmediatos

--- ARM_UNICICLO/extend.sv
+++ ARM_UNICICLO/extend.sv
@@ -8,14 +8,14 @@ module extend
 	logic [31:0] val;
 
 	assign shift = {Instr[11:8], 1'b0};
-	assign val = {24'b0, Instr[7:0]};
+	assign val   = {24'b0, Instr[7:0]};
 
 	always_comb
 		case(ImmSrc)
 						// 8-bit unsigned immediate
-			2'b00: 	ExtImm = {24'b0, Instr[7:0]};
+			2'b00: 	ExtImm = (val >> shift) | (val << (32 - shift));
 						// 12-bit unsigned immediate
-			2'b01: ExtImm = (val >> shift) | (val << (32 - shift));
+			2'b0x: ExtImm = (val >> shift) | (val << (32 - shift));
 		// 24-bit two's complement shifted branch
 			2'b10: 	ExtImm = {{6{Instr[23]}}, Instr[23:0], 2'b00};
 			default: ExtImm = 32'bx; // undefined
