52b0b29c3584223c8eb591ca52840ecf916a9709
heutlett <44324449+Heutlett@users.noreply.github.com>
2022-06-10 01:16:57 -0600

    update

--- ARM_UNICICLO/dmem.sv
+++ ARM_UNICICLO/dmem.sv
@@ -5,13 +5,13 @@ module dmem
 	output logic [31:0] rd
 );
 	
-	//initial
-	//	$readmemh("datamem2.dat",RAM);
+	initial
+		$readmemh("data_mem_init.dat",RAM);
 	
-	logic [7:0] RAM[127:0];
+	logic [31:0] RAM[127:0];
 	assign rd = RAM[a[31:2]]; // word aligned
 	
 	always_ff @(posedge clk)
-		if (we) RAM[a[31:2]] <= wd[7:0];
+		if (we) RAM[a[31:2]] <= wd;
 
 endmodule
