60e8237b8fa1bed26f3940859090a4eecbf4b8c2
alejandrodp <diaza913@gmail.com>
2022-06-11 13:21:19 -0600

    Agrega inicializador de ram

--- ARM_UNICICLO/dmem.sv
+++ ARM_UNICICLO/dmem.sv
@@ -5,12 +5,11 @@ module dmem
 	output logic [31:0] rd
 );
 	
-//	initial $readmemh("data_mem_init.dat",RAM);
+	initial $readmemh("data_mem_init.dat",RAM);
 	
 	logic [31:0] RAM[4095:0]; // 	512   	=>		0 hasta 2047 (dec)
 									 //				=>		000 hasta 7ff (hex)
 	
-<<<<<<< HEAD
 	always_ff @(posedge clk)
 		if (we) RAM[a[31:0]] <= wd;
 		else rd <= RAM[a[31:0]];
