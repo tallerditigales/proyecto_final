096e2b3b367d2908336b47e005f0a5d96cd525cf
heutlett <44324449+Heutlett@users.noreply.github.com>
2022-06-14 06:42:24 -0600

    xd

--- init_mem_script.py
+++ init_mem_script.py
@@ -1,13 +1,36 @@
-file = open("ARM_UNICICLO/data_mem_init.dat", "w") 
+import os
+from pathlib import Path
 
-texto = input("Ingrese el texto: ")
 
-for char in texto:
-    hex = "".join(["{:02x}".format(ord(c)) for c in char])
-    hex = "000000"+ str(hex) + "\n"
-    file.writelines(hex)
-file.write( "00000000")
+# create doc to insert phrase
+textFile = "phrase.txt"
+fle = Path(textFile)
+fle.touch(exist_ok=True)
+f = open(fle)
 
-file.close()
+#to current working directory
+save_path = "ARM_UNICICLO"
+file_name = "data_mem_init.dat"
+completeName = os.path.join(save_path, file_name)
+    
+## READ
+def read(file):
+    f = open(file, "r")
+    text = f.read()
+    f.close()
+    return text
 
-print(f"El texto tiene una longitud de: {len(texto)}")
+def script(text):
+    file = open(completeName, "w") 
+    # response = input("Digitar palabra ")
+    # texto = input("Ingrese el texto: ")
+    for char in text:
+        hex = "".join(["{:02x}".format(ord(c)) for c in char])
+        hex = "000000"+ str(hex) + "\n"
+        file.writelines(hex)
+    file.write( "00000000")
+    file.close()
+    print(f"El texto tiene una longitud de: {len(text)+1}")
+
+
+script(read(textFile))
